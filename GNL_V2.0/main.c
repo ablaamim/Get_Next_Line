fbcc5ea608b8a0fe8cd73a6bbff58dad4cd78ef6
ablaamim <aku-no-hana@outlook.fr>
2021-12-10 11:19:57 +0100

    main modif

--- GNL_V2.0/main.c
+++ GNL_V2.0/main.c
@@ -6,7 +6,7 @@
 /*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 05:09:31 by ablaamim          #+#    #+#             */
-/*   Updated: 2021/12/10 10:56:32 by ablaamim         ###   ########.fr       */
+/*   Updated: 2021/12/10 11:19:35 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,13 @@ int	main(int argc, char **argv)
 	(void)	argc;
 	(void)	argv;
 	char	*line;
-	int i = 0;
 	int	fd = open("test.txt", O_RDONLY);
-	while (i < 6)
+	line = get_next_line(fd);
+	while (line != NULL)
 	{
-		line = get_next_line(fd);
 		printf("%s", line);
 		free(line);
-		i++;
+		line = get_next_line(fd);
 	}
 	close (fd);
 	return (EXIT_SUCCESS);
