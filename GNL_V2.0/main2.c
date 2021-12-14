7805dead34cc5a89ddaee444574ff4dd669e6226
alaamimi <aku-no-hana@outlook.fr>
2021-12-14 12:38:00 +0100

    REMOVE

--- GNL_V2.0/main2.c
+++ GNL_V2.0/main2.c
@@ -6,7 +6,7 @@
 /*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 13:28:27 by ablaamim          #+#    #+#             */
-/*   Updated: 2021/12/10 13:35:34 by ablaamim         ###   ########.fr       */
+/*   Updated: 2021/12/10 13:44:21 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,20 @@
 int	main()
 {
 	int	fd = open("test.txt", O_RDONLY);
-	int	fd1 = open("test0.txt", O_RDONLY);
+//	int	fd1 = open("test0.txt", O_RDONLY);
 	char	*line = get_next_line_bonus(fd);
-	char	*line1 = get_next_line_bonus(fd1);
+//	char	*line1 = get_next_line_bonus(fd1);
 
-	while (line1 != NULL && line != NULL)
+	while (line != NULL)
 	{
 		printf("%s", line);
-		printf("%s", line1);
+//		printf("%s", line1);
 		free(line);
-		free(line1);
+//		free(line1);
 		line = get_next_line_bonus(fd);
-		line1 = get_next_line_bonus(fd1);
+//		line1 = get_next_line_bonus(fd1);
 	}
 	close(fd);
-	close(fd1);
+//	close(fd1);
 	return (EXIT_SUCCESS);
 }
